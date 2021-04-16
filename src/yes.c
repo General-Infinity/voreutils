8102758d971ad4cf74179062c679db734f7bef82
hiroshima <some@one.one>
2021-04-16 21:42:28 +0200

    fixed this shitwork of a code

--- src/yes.c
+++ src/yes.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char *argv[]){
+  char endres[500];
+
+  for (int i; i<argc; i++){
+    if(i!=0){
+      strcat(endres, argv[i]);
+      strcat(endres, " ");
+    }
+  } 
   if (!argv[1]){
     while(1){
       printf("y\n");
@@ -7,7 +16,7 @@ int main(int argc, char *argv[]){
   }
   else{
     while(1){
-      printf("%s\n",argv[1]);
+      printf("%s\n",endres);
     }
   }
   return 0;
